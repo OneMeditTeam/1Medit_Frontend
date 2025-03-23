ab874ad18a198606a698d606769b217454d1efd9
ohoCoding <youngho7482@gmail.com>
2025-03-23 23:13:11 +0900

    refactor: :recycle: Metadata 제목 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "루시트 eXpert",
+  title: "루시드 eXpert",
   description: "전문가 상담 서비스",
 };
 
